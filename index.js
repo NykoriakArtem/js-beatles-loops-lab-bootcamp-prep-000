a6b72d482acdc66b2c68136345b1d0b1a46a8d45
Artem Nykoriak <artemnykoriak@gmail.com>
2020-02-26 23:21:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ function theBeatlesPlay(musicians, instruments){
   const arr = [];
   
   for (let i = 0, l = musicians.length; i < l; i++) {
-    if (i === 1){
-      arr.push("John Lennon plays guitar")
-    }else{
       arr.push(`${musicians[i]} plays ${instruments}.`)
     }
-  }
   return arr[i];
 }
 
