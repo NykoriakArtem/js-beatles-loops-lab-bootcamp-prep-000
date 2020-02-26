3f49364866b788fb0d6cc730928703c1a93f48bf
Artem Nykoriak <artemnykoriak@gmail.com>
2020-02-26 23:16:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function theBeatlesPlay(musicians, instruments){
   const arr = [];
   
   for(let i=0; i < musicians.length; i++){
-    if (i === 1)
-    arr.push
+    if (i === 1){
+      arr.push(`${musicians[i]} plays ${instrument}.`)
+    }
   }
 }
 
